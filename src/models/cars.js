cdb507664afeceede72a5e5d15b2dccd39eef160
Mateus Adamo <adamomateus478@gmail.com>
2024-01-22 18:24:08 -0300

    feat: Post and Put  method of the created API.

--- src/models/cars.js
+++ src/models/cars.js
@@ -1,25 +1,3 @@
-const cars = [
-  {
-    brand: "Ferrari",
-    model: "488 GTB",
-    year: 2022,
-    color: "Red",
-    price: 300000,
-  },
-  {
-    brand: "Ferrari",
-    model: "F8 Tributo",
-    year: 2023,
-    color: "Yellow",
-    price: 350000,
-  },
-  {
-    brand: "Ferrari",
-    model: "Portofino",
-    year: 2021,
-    color: "Blue",
-    price: 250000,
-  },
-];
+const cars = [];
 
 module.exports = cars;
--- src/router.js
+++ src/router.js
@@ -4,7 +4,47 @@ const router = express.Router();
 const cars = require("./models/cars");
 
 router.get("/cars", (req, res) => {
-  res.status(200).json(cars);
+  if (cars.length == 0) {
+    res.status(404).json({ message: "Nenhum carro foi encontrado" });
+  } else {
+    res.status(200).json(cars);
+  }
+});
+
+router.post("/cars", (req, res) => {
+  const newCar = req.body;
+
+  //adicionar uma ID única pra cada carro adicionado!!
+  const nextId =
+    cars.length > 0 ? Math.max(...cars.map((car) => car.id)) + 1 : 1;
+  newCar.id = nextId;
+
+  cars.push(newCar);
+
+  res.status(200).json(newCar);
+});
+
+router.put("/cars/:id", (req, res) => {
+  const carId = parseInt(req.params.id);
+  const updatedCar = req.body;
+
+  console.log("Requisição PUT recebida para:", `/cars/${carId}`);
+  console.log("Dados Atualizados:", updatedCar);
+
+  const carIndex = cars.findIndex((car) => car.id === carId);
+
+  console.log("Índice do Carro:", carIndex);
+
+  if (carIndex !== -1) {
+    cars[carIndex] = { ...cars[carIndex], ...updatedCar };
+    res.json({
+      success: true,
+      message: "Carro atualizado com sucesso",
+      data: cars[carIndex],
+    });
+  } else {
+    res.status(404).json({ success: false, message: "Carro não encontrado" });
+  }
 });
 
 module.exports = router;
